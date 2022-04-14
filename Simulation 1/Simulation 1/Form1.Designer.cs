a212014ff8caf09113fa612e8f87e2a4b46baff2
Hrittik <49366463+Hrittik20@users.noreply.github.com>
2022-04-14 14:11:06 +0530

    C# program for simulating flight in the atmosphere

---
+++ Simulation 1/Simulation 1/Form1.Designer.cs
@@ -0,0 +1,293 @@
+﻿
+namespace Simulation_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btLaunch = new System.Windows.Forms.Button();
+            this.edWeight = new System.Windows.Forms.NumericUpDown();
+            this.edSize = new System.Windows.Forms.NumericUpDown();
+            this.edSpeed = new System.Windows.Forms.NumericUpDown();
+            this.edAngle = new System.Windows.Forms.NumericUpDown();
+            this.edHeight = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edWeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edAngle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edHeight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.AxisX.Maximum = 40D;
+            chartArea3.AxisX.Minimum = 0D;
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            this.chart1.Location = new System.Drawing.Point(0, 120);
+            this.chart1.Name = "chart1";
+            series3.BorderWidth = 4;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(895, 506);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btLaunch);
+            this.panel1.Controls.Add(this.edWeight);
+            this.panel1.Controls.Add(this.edSize);
+            this.panel1.Controls.Add(this.edSpeed);
+            this.panel1.Controls.Add(this.edAngle);
+            this.panel1.Controls.Add(this.edHeight);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(895, 129);
+            this.panel1.TabIndex = 1;
+            // 
+            // btLaunch
+            // 
+            this.btLaunch.Location = new System.Drawing.Point(668, 25);
+            this.btLaunch.Name = "btLaunch";
+            this.btLaunch.Size = new System.Drawing.Size(190, 71);
+            this.btLaunch.TabIndex = 10;
+            this.btLaunch.Text = "Launch";
+            this.btLaunch.UseVisualStyleBackColor = true;
+            this.btLaunch.Click += new System.EventHandler(this.btLaunch_Click);
+            // 
+            // edWeight
+            // 
+            this.edWeight.Location = new System.Drawing.Point(455, 53);
+            this.edWeight.Name = "edWeight";
+            this.edWeight.Size = new System.Drawing.Size(136, 22);
+            this.edWeight.TabIndex = 9;
+            this.edWeight.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.edWeight.ValueChanged += new System.EventHandler(this.numericUpDown5_ValueChanged);
+            // 
+            // edSize
+            // 
+            this.edSize.DecimalPlaces = 1;
+            this.edSize.Location = new System.Drawing.Point(455, 15);
+            this.edSize.Name = "edSize";
+            this.edSize.Size = new System.Drawing.Size(136, 22);
+            this.edSize.TabIndex = 8;
+            this.edSize.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            131072});
+            // 
+            // edSpeed
+            // 
+            this.edSpeed.Location = new System.Drawing.Point(106, 92);
+            this.edSpeed.Name = "edSpeed";
+            this.edSpeed.Size = new System.Drawing.Size(136, 22);
+            this.edSpeed.TabIndex = 7;
+            this.edSpeed.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // edAngle
+            // 
+            this.edAngle.Location = new System.Drawing.Point(106, 50);
+            this.edAngle.Name = "edAngle";
+            this.edAngle.Size = new System.Drawing.Size(136, 22);
+            this.edAngle.TabIndex = 6;
+            this.edAngle.Value = new decimal(new int[] {
+            45,
+            0,
+            0,
+            0});
+            // 
+            // edHeight
+            // 
+            this.edHeight.Location = new System.Drawing.Point(106, 15);
+            this.edHeight.Name = "edHeight";
+            this.edHeight.Size = new System.Drawing.Size(136, 22);
+            this.edHeight.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(361, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Weight";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(361, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Size";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Speed";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Angle";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Height";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(952, 147);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(89, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(953, 197);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(87, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(952, 248);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(87, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(952, 295);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(87, 22);
+            this.textBox4.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1046, 631);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edWeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edAngle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edHeight)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown edWeight;
+        private System.Windows.Forms.NumericUpDown edSize;
+        private System.Windows.Forms.NumericUpDown edSpeed;
+        private System.Windows.Forms.NumericUpDown edAngle;
+        private System.Windows.Forms.NumericUpDown edHeight;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btLaunch;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Simulation 1/Simulation 1/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simulation_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown5_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        const double g = 9.81;
+        const double dt = 0.01;
+        const double rho = 1.29;
+        const double C = 0.15;
+
+        double height;
+        double speed;
+        double sina;
+        double cosa;
+        double t;
+        double size;
+        double weight;
+        double beta;
+        double k;
+        double max_y;
+
+        double vx, vy, vx_old, vy_old;
+        double x, y;
+
+        private void btLaunch_Click(object sender, EventArgs e)
+        {
+            height = (double)edHeight.Value;
+            double angle = (double)edAngle.Value;
+            speed = (double)edSpeed.Value;
+            size = (double)edSize.Value;
+            weight = (double)edWeight.Value;
+
+            sina = Math.Sin(angle * Math.PI / 180);
+            cosa = Math.Cos(angle * Math.PI / 180);
+
+            beta = 0.5 * C * size * rho;
+            k = beta / weight;
+
+            vx = speed * cosa;
+            vy = speed * sina;
+
+            chart1.Series[0].Points.Clear();
+
+            t = 0;
+            x = 0;
+            y = height;
+            max_y = y;
+
+            chart1.Series[0].Points.AddXY(x, y);
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            t += dt;
+            double v = (double)Math.Sqrt((double)(vx * vx + vy * vy));  
+            vx = vx - k * vx * v * dt;
+            vy = vy - (g + k * vy * v) * dt;
+            x = x + vx * dt;
+            y = y + vy * dt;
+            if (max_y<y)
+            {
+                max_y = y;
+            }
+            chart1.Series[0].Points.AddXY(x, y);
+
+
+            if (y <= 0)
+            {
+                textBox1.Text = dt.ToString();
+                textBox2.Text = Math.Round(x,2).ToString();
+                textBox3.Text = Math.Round(max_y,2).ToString();
+                textBox4.Text = Math.Round(v, 2).ToString();
+                timer1.Stop();
+            }
+        }
+    }
+}
